7c6d51ce00fa078f71e3076185f159213a686b29
Anthony Coots <anthonyjcoots@yahoo.com>
2026-01-27 08:25:23 -0800

    Added first example from chapter 4.

---
+++ Chapter 4/datasets-first.py
@@ -0,0 +1,43 @@
+"""
+    Python Interpreter version: 3.11
+    TensorFlow version: 2.20.0
+    TensorFlow-addons version: 0.22.0
+    TensorFlow datasets version: 4.9.9
+"""
+
+import tensorflow as tf
+import tensorflow_datasets as tfds
+
+print('tf.__version__:', tf.__version__)
+print('tfds.__version__:', tfds.__version__)
+
+(training_images, training_labels), (test_images, test_labels) = tfds.as_numpy(
+    tfds.load(
+        'fashion_mnist',
+        split=['train', 'test'],
+        batch_size=-1,
+        as_supervised=True,
+    )
+)
+
+training_images = training_images / 255.
+test_images = test_images / 255.
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28,28,1)),
+    tf.keras.layers.Dense(128,activation='relu'),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(10,activation='softmax')
+])
+
+model.compile(
+    optimizer='adam',
+    loss='sparse_categorical_crossentropy',
+    metrics=['accuracy']
+)
+
+model.fit(
+    training_images,
+    training_labels,
+    epochs=5
+)
