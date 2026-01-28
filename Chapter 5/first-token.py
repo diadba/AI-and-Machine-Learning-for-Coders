099fd334a142236696a504f58f4c50537907921e
diadba <anthonyjcoots@yahoo.com>
2026-01-28 14:20:52 -0800

    Added first example from chapter 5.

---
+++ Chapter 5/first-token.py
@@ -0,0 +1,27 @@
+"""
+    Python Interpreter version: 3.11
+    TensorFlow version: 2.15.0
+"""
+from typing import Any
+from tensorflow.keras.preprocessing.text import Tokenizer
+
+sentences = [
+    'Today is a sunny day',
+    'Today is a rainy day'
+]
+
+tokenizer=Tokenizer(num_words=100)
+tokenizer.fit_on_texts(sentences)
+word_index: dict[Any, Any]=tokenizer.word_index
+print('First iteration:', word_index)
+
+sentences = [
+    'Today is a sunny day',
+    'Today is a rainy day',
+    'Is it sunny today?'
+]
+
+tokenizer=Tokenizer(num_words=100)
+tokenizer.fit_on_texts(sentences)
+word_index: dict[Any, Any]=tokenizer.word_index
+print('Second iteration:', word_index)
